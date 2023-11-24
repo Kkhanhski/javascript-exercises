aba7c0db442f490591b230fae5f106d7236af11e
Khanhski <cucskho@gmail.com>
2023-11-24 11:45:01 +0700

    Complete ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if(times < 0) return 'ERROR'
+    else {
+        var str = "";
+        for(var i=0; i<times; i++) {
+            str+= word;
+        }
+        return str;
+    }
 };
 
 // Do not edit below this line
