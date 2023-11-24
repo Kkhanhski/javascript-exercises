0e92b002c0364a342d70424cef6aac8e92edd591
Khanhski <cucskho@gmail.com>
2023-11-24 12:05:06 +0700

    Complete exercises
    OK

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var arr = [];
+    var char='';
+    var strReverse = '';
+    for(var index = 0; index < str.length; index++) {
+        char = str.charAt(index);
+        arr.unshift(char);
+    }
+    strReverse = arr.join('');
+    return strReverse;
 };
 
 // Do not edit below this line
