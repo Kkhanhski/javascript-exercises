567f6402a81f4225611b605c10c26b2aff67c866
Khanhski <cucskho@gmail.com>
2023-11-30 09:37:43 +0700

    chua hoan thanh

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...restParam) {
+    for(var i = 0; i < arr.length; i++) {
+        for(j of restParam) {
+            if(arr[i] === j) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
