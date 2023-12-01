15d04a77e328fbcb71c4242a079e5bdaccc6061a
Khanhski <cucskho@gmail.com>
2023-12-01 21:19:32 +0700

    Complete ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  var c = 0;
+  c = (f - 32) * (5/9);
+  return Number(c.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  var f = 0;
+  f = c * (9/5) + 32;
+  return Number(f.toFixed(1));
 };
 
 // Do not edit below this line
