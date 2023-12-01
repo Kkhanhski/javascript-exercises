a19e1b25a91b8a82836251ddf1df5d6d247ea20f
Khanhski <cucskho@gmail.com>
2023-12-01 20:37:24 +0700

    Complete ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    var result = 0;
+    if((typeof(a) === 'number' && typeof(b) === 'number') && a >= 0 && b >= 0) {
+        if(a > b) {
+            let c = a;
+            a = b;
+            b = c;
+        }
+        for(a; a<=b; a++) {
+            result+= a;
+        }
+    } else return 'ERROR';
+    return result;
 };
 
 // Do not edit below this line
